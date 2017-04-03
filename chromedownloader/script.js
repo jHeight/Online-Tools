5576d45ca1a975a50dca8b8cb699a81d852a0b72
SlipOnSerum      Verified ✅ <matthew.ronchetto678@icloud.com>
2017-04-03 08:24:06 -0700

    Update script.js

--- chromedownloader/script.js
+++ chromedownloader/script.js
@@ -1,5 +1,5 @@
 function main(){
 	var extensionID = document.getElementById('ccCallbox').value;
 	$('a#ccCallbutton').attr({target: '_blank', 
-                    href  : 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+extensionID+'%26installsource%3Dondemand%26uc'});
+                    href  : '//clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+extensionID+'%26installsource%3Dondemand%26uc'});
 }
